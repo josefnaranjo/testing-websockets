51adabe8be486934cdb7b2e98f7c0cd04f16376a
Teddy <ted.j.gallion@gmail.com>
2024-04-15 14:54:01 -0700

    user dm bar added

--- Front/app/page.tsx
+++ Front/app/page.tsx
@@ -1,10 +1,26 @@
+
+'use client';
+import React, { useState } from "react";
+import UserList, {generateUserList} from "../dm-list/dm-list"; // Importing UserList component
 export default function Home() {
+
+  // Assuming you have some userList data here
+  const userList = generateUserList(13);
   return (
     <div className="project-container">
       <nav className="navbar">Navbar</nav>
       <div className="main-rows">
-        <div className="side-channels-container"></div>
-        <div className="direct-messages-container"></div>
+        <div className="side-channels-container">
+          <div className="messages-bar">
+              <div className="w-[360px] h-[74px] left-0 top-0 absolute border-opacity-25">
+              </div>
+          </div>
+        </div>
+        <div className="direct-messages-container relative">
+      
+          {/* Render UserList component */}
+          <UserList userList={userList} />
+        </div>
         <div className="message-log-container"></div>
       </div>
     </div>
