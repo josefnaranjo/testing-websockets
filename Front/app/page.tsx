72ec8ed7b012c21ee8717f352b83fec3e776d30a
Teddy <ted.j.gallion@gmail.com>
2024-04-15 20:10:29 -0700

    you know i had to do it to em

--- Front/app/page.tsx
+++ Front/app/page.tsx
@@ -1,5 +1,6 @@
 import SideBar from "@/app/components/SideBar/SideBar";
-
+import UserList, {generateUserList} from "./components/dm-list/dm-list"
+const userList = generateUserList(13);
 export default function Home() {
   return (
     <div className="project-container">
@@ -10,7 +11,10 @@ export default function Home() {
           <SideBar />
         </div>
 
-        <div className="direct-messages-container"><h1>Side Messages</h1></div>
+        <div className="direct-messages-container">
+          <h1>Side Messages</h1>
+          <UserList userList={userList} />
+          </div>
         <div className="message-log-container"><h1>Message Log</h1></div>
       </div>
     </div>
