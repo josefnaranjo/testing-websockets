d3a72284a1315036654c24c3d315d9fcfda6a20c
DArlegui <79351401+DArlegui@users.noreply.github.com>
2024-05-10 14:05:07 -0700

    54 refactor direct messages container (#55)
    * Refactor/Redesign direct-messages-container to a more modern design
    
    * Final Prisma Schema Design
    
    * Add dummy icons for testing purposes
    
    * chore: Update user icons and offline state color

--- Front/app/components/dm-list/dm-list.tsx
+++ Front/app/components/dm-list/dm-list.tsx
@@ -1,30 +1,36 @@
 import Image, { StaticImageData } from "next/image";
-import smolSkul from "./smolskul.png";
-import './dm-list.css'
+import { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
+import "./dm-list.css";
+import icon1 from "./icon.png";
+import icon2 from "./icon2.jpg";
+import icon3 from "./icon3.png";
+import icon4 from "./icon4.jpg";
+import icon5 from "./icon5.png";
 
 type User = {
   name: string;
   icon: StaticImageData;
-  activeState: 'active' | "away" | "offline";
+  activeState: "active" | "away" | "offline";
 };
 
 export function generateUserList(numUsers: number): { [key: string]: User } {
   let userList: { [key: string]: User } = {};
+  let userIconArray = [icon1, icon2, icon3, icon4, icon5];
 
   for (let i = 1; i <= numUsers; i++) {
-    let activeState: User['activeState'] = 'active'; // Default to 'active'
+    const states = ["active", "away", "offline"];
+    const activeState = states[
+      Math.floor(Math.random() * states.length)
+    ] as User["activeState"];
 
-    if (i > 5) {
-      activeState = 'away';
-    }
-    if (i > 8) {
-      activeState = 'offline';
-    }
+    const iconIndex = Math.floor(Math.random() * userIconArray.length);
+    const icon = userIconArray.splice(iconIndex, 1)[0]; // This removes the icon from the array and returns it
 
     let user: User = {
       name: `user${i}`,
-      icon: smolSkul,
-      activeState: activeState
+      icon: icon,
+      activeState: activeState,
     };
 
     userList[`user${i}`] = user;
@@ -32,34 +38,81 @@ export function generateUserList(numUsers: number): { [key: string]: User } {
 
   return userList;
 }
-const getColorFromState = (state: 'active' | 'away' | 'offline'): string => {
-    switch (state) {
-      case 'active':
-        return 'green';
-      case 'away':
-        return 'yellow';
-      case 'offline':
-        return 'red';
-      default:
-        return ''; 
-    }
-  };
-const UserList: React.FC<{ userList: { [key: string]: User } }> = ({ userList }) => {
-    return (
-      <div className="list-container">
-        <h4>Chats</h4>
-        <ul className="user-list">
-          {Object.values(userList).map(user => (
-            <li key={user.name} className="user-item">
-              <Image src={user.icon} alt={user.name} width={40} height={40} className="user-icon" />
-              <span className="user-name">{user.name}</span>
-              {/* Conditionally set background color */}
-              <div className={` user-state`} style={{backgroundColor:`${getColorFromState(user.activeState)}`}}></div>
-            </li>
-          ))}
-        </ul>
+
+const getColorFromState = (state: "active" | "away" | "offline"): string => {
+  switch (state) {
+    case "active":
+      return "green";
+    case "away":
+      return "yellow";
+    case "offline":
+      return "gray";
+    default:
+      return "";
+  }
+};
+
+const UserList: React.FC<{ userList: { [key: string]: User } }> = ({
+  userList,
+}) => {
+  const [selected, onSelected] = useState<string | null>(null);
+
+  return (
+    <div className="list-container">
+      <div className="flex items-center h-[63px] border-b border-gray-400/opacity-25">
+        <h4 className="font-semibold text-3xl pr-[10px] pl-[16px]">Chat</h4>
+        {/* <FaRegCircle size={25} className="mt-1" /> */}
       </div>
-    );
-  };
+      <ul className="user-list">
+        {Object.values(userList).map((user) => (
+          <li
+            key={user.name}
+            className={`user-item transition-colors duration-200 cursor-pointer ${
+              selected === user.name
+                ? "group bg-gray-200 "
+                : "group hover:bg-gray-200"
+            }`}
+            style={{ maxWidth: "360px" }}
+            onClick={() =>
+              onSelected(user.name === selected ? null : user.name)
+            }
+          >
+            <div className="user-icon">
+              <Image
+                src={user.icon}
+                alt={user.name}
+                fill={true}
+                className="object-contain"
+              />
+            </div>
+            <div className="flex flex-col w-full pl-3 max-w-[272px]">
+              <span className="user-name">{user.name}</span>
+              <p className="truncate text-sm">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
+                ab quasi soluta
+              </p>
+            </div>
+            {/* Conditionally set background color */}
+            <div
+              className="user-state absolute bottom-3 left-12"
+              style={{
+                backgroundColor: `${getColorFromState(user.activeState)}`,
+              }}
+            />
+            <button
+              className="absolute top-2 right-2 cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity duration-300"
+              onClick={() => console.log("clicked")}
+            >
+              <RxCross2
+                className="hover:text-red-500"
+                style={{ width: "18px", height: "18px" }}
+              />
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default UserList;
--- Front/app/page.tsx
+++ Front/app/page.tsx
@@ -1,31 +1,32 @@
-'use client';
-import MessageLog from "./MessageLog"
+"use client";
+import MessageLog from "./MessageLog";
 import React, { useState } from "react";
 import SideBar from "./components/SideBar/SideBar";
-import UserList, {generateUserList} from "./components/dm-list/dm-list";
+import UserList, { generateUserList } from "./components/dm-list/dm-list";
 
 export default function Home() {
-
- 
-  const userList = generateUserList(8);
+  const userList = generateUserList(5);
 
   return (
-    <div className="project-container">
-      <div className="main-rows">
+    <div
+      className="project-container"
+      // style={{
+      //   backgroundImage: "linear-gradient(120deg, #84fab0 0%, #8fd3f4 100%)",
+      // }}
+    >
+      <div className="main-rows bg-white">
         <div className="side-channels-container">
           <SideBar />
         </div>
         <div className="messages-bar">
-              <div className="w-[360px] h-[74px] left-0 top-0 absolute border-opacity-25">
-              </div>
-          </div>
+          <div className="w-[360px] h-[74px] left-0 top-0 absolute border-opacity-25"></div>
+        </div>
         <div className="direct-messages-container relative">
-      
           {/* Render UserList component */}
           <UserList userList={userList} />
         </div>
         <div className="message-log-container">
-            <MessageLog />
+          <MessageLog />
         </div>
       </div>
     </div>
