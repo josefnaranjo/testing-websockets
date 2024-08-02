da30be14d745e86ece58a659e079f6c85560edea
josefnaranjo <jfnaappus@gmail.com>
2024-08-01 19:37:45 -0700

    updated0

--- Front/lib/db.ts
+++ Front/lib/db.ts
@@ -4,9 +4,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
-//Export a singleton instance of PrismaClient
-export const db = globalThis.prisma || new PrismaClient();
+export const db = global.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  globalThis.prisma = db;
+  global.prisma = db;
 }
